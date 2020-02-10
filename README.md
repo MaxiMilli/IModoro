b6f885f45e1c80220551a1ba7acd2b9b4de8a777
MaxiMilli <briefkasten@gleis6.ch>
2020-02-10 15:21:55 +0100

    Anpassungen gemäss Meeting

--- README.md
+++ README.md
@@ -4,3 +4,23 @@ IModoro ist die Pomodoro-Übung um im 2. Semester MMP JavaScript zu lernen.
 
 Programmiert nach den FHGR [Coding-Conventions](https://github.com/MartinVollenweider/MMP-Coding-Conventions)
 Demo: [FHGR-Ausbildungsserver](https://446808-1.web1.fh-htwchur.ch)
+
+## Gesprächsnotizen
+
+
+
+- Objekte zuerst bauen und erst nachher in Klammern einfügen
+- Alle Timerfunktionen im Javascript möglich (auf W3Schools gibt es die bEfehle) --> Idee der Bibliothek vermitteln
+- Timerbibliothek drin lassen
+- Modul aufsplitten in verschiedene Funktionen
+- local Storage party vergleich
+- html zusammenbauen
+- überall "let"
+- Formulare erklären und im minimum behalten
+- camel case anpassen
+- css kein mitliefern
+- Grundstruktur mit sections, h1, form, ect.
+- keine Klassen und IDs
+- struktur einrichten --> Webserver
+- kein normalize
+- file immer auf webserver
--- js/control.js
+++ js/control.js
@@ -2,31 +2,150 @@
 // *      index         *
 // **********************
 
-// Wenn das Dokument geladen wurde, führe loadFunc() aus
+// Wenn das Dokument geladen wurde, führe die Funktion 'loadFunc' aus
 document.addEventListener('DOMContentLoaded', loadFunc);
 
-// Lade und zeige alle Objekte
+// Globale Variablen
+let timer;
+let name;
+let beschreibung;
+
+// Führe aus, sobald die Webseite fertig gealden hat
 function loadFunc() {
-    // Das Registrationsformular
-    var pomoListe = document.querySelector('#pomodoro_liste');
-    if (pomoListe) {
-        // Rufe die Funktion 'schreibeListe' aus dem Modul auf und übergebe ein Template.
-        pomoModule.schreibeListe('pomodoro_liste', `
-        <div class="liste__eintrag">
-            <div class="liste__titel">{{title}}</div>
-            <div class="liste__untertitel">{{subtitle}}</div>
-        </div>
-        `);
-    }
+    // Blende den Timer aus, da noch keiner gestartet wurde
+    document.querySelector('#pomodoroTimer').style.display = 'none'
 
-    var pomoNeu = document.querySelector('#pomodoro_neu_formular');
-    if (pomoNeu) {
-        pomoNeu.addEventListener('submit', pomoModule.startNeu);
-    }
+    // Füge den Eventlistener zu, welcher auf das Formular hört
+    let formular = document.querySelector('#pomodoroNeuFormular');
+    formular.addEventListener('submit', neuerPomodoroStarten);
 
-    let pomoTimer = document.querySelector('#pomodoro_timer');
-    if (pomoTimer) {
-        pomoModule.timer()
-        document.querySelector('#pomo_abbrechen').addEventListener('click', pomoModule.abbrechen)
+    // Erstelle die Liste mit allen vergangenen Pomodoros
+    erstelleListe()
+}
+
+function erstelleListe() {
+    let pomodoroSammlung = JSON.parse(localStorage.getItem('pomodoro'));    
+    for (pomodoro in pomodoroSammlung) {
+        erstelleListenEintrag(pomodoroSammlung[pomodoro]);
     }
-} // Ende loadFunc
+}
+
+function neuerPomodoroStarten(paramEvent) {
+    // hindere JavaScript vor allen standardmässigen Befehlen
+    paramEvent.preventDefault()
+
+    // verändere die Ansicht der Sections
+    document.querySelector('#pomodoroTimer').style.display = 'block';
+    document.querySelector('#pomodoroNeu').style.display = 'none';
+
+    // Starte den Pomodoro und führe diese Funktion aus
+    startePomodoro()
+}
+
+function erstelleListenEintrag(paraPomodoroObjekt) {
+
+    // selektiere das Listenfeld
+    let liste = document.querySelector("#pomodoroListe");
+    
+    // Erstelle einen neuen Artikel (ein Artikel ist bei uns einen Pomodoro in der Liste)
+    let eintrag = document.createElement('article');
+    
+    // Erstelle den Titel mit einem 'h4'-Element
+    let eintragTitel = document.createElement('h4');
+    eintragTitel.innerText = paraPomodoroObjekt.name; // Gib dem Titel den Titel-Text, welcerh übergeben wurde
+    eintrag.appendChild(eintragTitel); // füge den Titel zum Artikel hinzu
+    
+    // Erstelle den Untertitel als Paragraph
+    let eintragBemerkung = document.createElement('p');
+    let eintragBemerkungText = paraPomodoroObjekt.beschr; // Text des Paragraphen
+
+    // füge einen Text hinten an, je nach dem ob der Pomodor fertig gemacht wurde
+    if (paraPomodoroObjekt.fertig) {
+        eintragBemerkungText += " | fertig";
+    } else {
+        eintragBemerkungText += " | abgebrochen";
+    }
+    eintragBemerkung.innerText = eintragBemerkungText;
+    eintrag.appendChild(eintragBemerkung); // füge die Bemerkung zum Artikel hinzu
+    
+    // Element in der Liste am Schluss einfügen
+    liste.appendChild(eintrag); 
+}
+
+function startePomodoro() {
+    // Erstelle einen Timer
+    timer = new easytimer.Timer();
+    timer.start({countdown: true, startValues: {seconds: 10, minutes: 0}});
+    timer.on('secondsUpdated', timerTick)
+    timer.on('targetAchieved', timerFinish);
+
+    // Hole die Werte aus dem Formular
+    name = document.querySelector('#uebungName').value
+    beschreibung = document.querySelector('#uebungBeschreibung').value
+
+    //Abbrechen vorbereiten
+    let abbruchFormular = document.querySelector('#pomodoroAbbruchFormular');
+    abbruchFormular.addEventListener('submit', brechePomodoroAb)
+}
+
+function brechePomodoroAb() {
+    // Timer beenden
+    timer.stop();
+    timer.reset();
+
+    // PomodoroTimer zurücksetzen
+    document.querySelector('#pomoBalken').style.width = '0%';
+    document.querySelector('.timer__zeit').innerHTML = '..:..';
+
+    // Ansicht wechseln
+    document.querySelector('#pomodoroTimer').style.display = 'none';
+    document.querySelector('#pomodoroNeu').style.display = 'block';
+
+    // Pomodoro eintragen
+    pomodoroEintragen(false)
+}
+
+function timerTick() {
+    // verändere den Zeit-Text
+    document.querySelector('.timer__zeit').innerHTML = timer.getTimeValues().minutes + ":" + timer.getTimeValues().seconds;
+    
+    // verändere den Balken
+    let prozentFortschritt = 100 / 25 * timer.getTimeValues().minutes; // Prozent in Miuten ausrechnen
+    prozentFortschritt = 100 - prozentFortschritt; // Prozentzahl umkehren
+    document.querySelector('#pomoBalken').style.width = prozentFortschritt + "%"; // Auf Balken anwenden
+}
+
+function timerFinish() {
+    // Pomodoro speichern (true, weil der Pomodoro erfolgreich beendet wurde)
+    pomodoroEintragen(true);
+
+    // Titel verändern
+    document.querySelector('.timer__zeit').innerHTML = 'Fertig!';
+    
+    // Player konfigurieren
+    let audioObject = document.querySelector('#audioPlayer');
+    audioObject.play();
+    audioObject.onended = function() {
+        location.reload();
+    };
+}
+
+function pomodoroEintragen(paramComplete) {
+    // Zeit und localStorage in Variablen speichern
+    let zeitInSekunden = new Date().getTime();
+    let pomodoroSammlung = JSON.parse(localStorage.getItem('pomodoro'));
+
+    // Pomodoro-Objekt erstellen
+    let neuerEintragObjekt = {
+        name: name,
+        beschr: beschreibung,
+        zeitstempel: zeitInSekunden,
+        fertig: paramComplete
+    };
+
+    // Füge den neuen Pomodoro dem localStorage-Array an
+    pomodoroSammlung.push(neuerEintragObjekt);
+
+    // speichere das die ganze Sammlung
+    localStorage.setItem('pomodoro', JSON.stringify(pomodoroSammlung));
+}
